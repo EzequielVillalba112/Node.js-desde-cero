13e554f0b9938f85f98bf8cf6ca9d6ab56129c32
EzequielVillalba112 <148280303+EzequielVillalba112@users.noreply.github.com>
2023-12-04 17:16:26 -0300

    1

--- Desarrollo-de-Api/1.http.js
+++ Desarrollo-de-Api/1.http.js
@@ -1,12 +1,23 @@
-const http = require('node:http');
+const http = require("node:http");
 
 const port = process.env.PORT ?? 3000;
 
-const server = http.createServer((req, res) => {
-    console.log('request received: ', req.url);
-    res.end("Holas...");
-})
+const processRequest = (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("<h1>pagina inicial</h1>");
+  } else if (req.url === "/inicio") {
+    res.statusCode = 200;
+    res.end("<h1>inicio</h1>");
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>Not Found 404</h1>");
+  }
+};
 
-server.listen(port, ()=>{
-    console.log(`server listening on port http://localhost:${port}`);
-})
+const server = http.createServer(processRequest);
+
+server.listen(port, () => {
+  console.log(`server listening on port http://localhost:${port}`);
+});
